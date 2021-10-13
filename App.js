645a76e756c645d41a72ce44ec29981fb6652aa0
Shad Sylla <34220082+shadsylla@users.noreply.github.com>
2021-10-13 11:55:18 -0500

    website in react.js

---
+++ App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './App.css'; 
+import Home from '../Home/'; 
+
+import { BrowserRouter as Router } from 'react-router-dom'; 
+
+
+function App() {
+ 
+
+
+   return(
+ 
+      <Router> 
+         <Home /> 
+      </Router>
+); 
+
+}
+
+export default App; 
